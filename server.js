11ecc5fadc8c6366f4773f9e2fcc119f003f6a44
natashaswan <91715868+natashaswan@users.noreply.github.com>
2022-02-16 12:55:45 -0800

    initial commit with node skeleton

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const bodyParser = require("body-parser");
+
+const express = require("express");
+
+//local files
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+//helpers
+const rootDir = require("./utils/path");
+
+const path = require("path");
+
+const app = express();
+app.use(express.static('public'));
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next)=>{
+    res.status(404).sendFile(path.join(rootDir, "views", "404-error.html"));
+})
+
+
+app.listen(3001, ()=>{
+    console.log("working...")
+});
