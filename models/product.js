ab5fcd24a220346ee2da17008414631bdd9ea8c4
natashaswan <91715868+natashaswan@users.noreply.github.com>
2022-02-22 13:31:34 -0800

    fixed bug with saving to file, now new file is created if did not exist, products appended not overwritten

--- models/product.js
+++ models/product.js
@@ -1,7 +1,13 @@
 const path = require("path");
 const fs = require("fs");
+const { stringify } = require("querystring");
 //const pathUtil = require("../utils/path")
-
+const dataPath = path.join(
+    __dirname,
+    "../", 
+    "data", 
+    "/products.json"
+);
  
 
 module.exports = class Product {
@@ -11,35 +17,41 @@ module.exports = class Product {
         this.description = _description;
     }
     saveProducts(){  
-        const dataPath = path.join(
-            __dirname,
-            "../", 
-            "data", 
-            "/products.json"
-            );
-        console.log(dataPath); 
-
-        fs.readFile(dataPath, (err, fileData)=>{            
-            let products = [];
+        const products = [];
+        
+        fs.readFile(dataPath, (err, fileData)=>{
             if(!err){
-                products = JSON.parse(fileData);
+                products.push(JSON.parse(fileData));
+                
             }
-            products.push(this);
-            fs.writeFile(dataPath, JSON.stringify(products), (err)=>{
+            products.push(this)
+            fs.writeFile(dataPath, JSON.stringify(products, null, 2), {flag: "w"}, (err)=>{
                 console.log(err);
-            });
-        });      
+            })
+           
+        })
+        
+
+
+
+        // fs.readFile(dataPath, (err, fileData)=>{            
+        //     let products = [];
+        //     products.push(this);
+        //     if(!err){
+        //         return products = JSON.parse(fileData);
+        //     }
+            
+        //     fs.writeFile(dataPath, JSON.stringify(products), (err)=>{
+        //         console.log(err);
+        //     });
+        // });      
     }
     static fetchProducts(cb){
-        const dataPath = path.join(
-            __dirname,
-            "../", 
-            "data", 
-            "/products.json"
-        );
+        console.log("trying to read data")
+        console.log(dataPath.length)
         fs.readFile(dataPath, (err, fileData)=>{
-            if (err){
-                cb([]);
+            if (err || dataPath.length === 0){
+                return cb([]);
             }
            cb(JSON.parse(fileData));
     
